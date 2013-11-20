23d1209a15ce6b59de25d8bb14522b14c4edb8e7
JoshWeaver <weaverj8@students.wwu.edu>
2013-11-19 23:06:08 -0800

    Added InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,35 @@
+public class insertionsort{
+
+	public static int[] array = {5,6,4,7,3,8,2,9,1};
+	public static int temp;
+
+	public static void main(String[] args){
+		System.out.print("Original list: ");
+		for (int k = 0; k < array.length; k++){
+				System.out.print(array[k]);
+				System.out.print(" ");
+		}
+		System.out.println();
+		System.out.println("Sorting... ");
+		
+		for (int i = 1; i < array.length; i ++){
+			int element = array[i];
+			int j = i;
+			while(j > 0 && element < array[j - 1]){
+				array[j] = array[j - 1];
+				j --;
+			}
+			array[j] = element;
+			for (int k = 0; k < array.length; k++){
+				System.out.print(array[k]);
+			}
+			System.out.println();
+		} 
+		
+		System.out.print("Final list: ");
+		for (int k = 0; k < array.length; k++){
+				System.out.print(array[k]);
+				System.out.print(" ");
+		}
+	}
+}
